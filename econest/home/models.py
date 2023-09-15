f26081c044b702032166710c84236d9e3dbef5b5
Snapman5678 <89893519+Snapman5678@users.noreply.github.com>
2023-09-16 02:38:51 +0530

    Update models.py

--- econest/home/models.py
+++ econest/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Neighbourhood_Info(models.Model):
+    average_rent=models.IntegerField(default=0)
+    neighbourhood= models.CharField(max_length=30,verbose_name="Neighbourhood")
+
+
+    def __str__(self):
+        return self.neighbourhood
