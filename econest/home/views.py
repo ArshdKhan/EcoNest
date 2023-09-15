54fb9771e3208afd5f48b69d83880b935674205c
Snapman5678 <89893519+Snapman5678@users.noreply.github.com>
2023-09-16 02:38:25 +0530

    Update views.py

--- econest/home/views.py
+++ econest/home/views.py
@@ -14,3 +14,22 @@ def search(request):
 def maps(request):
     return render(request, "maps.html")
 
+def maps(request, map_id):
+    try:
+        neighbourhood_info = Neighbourhood_Info.objects.get(neighbourhood=map_id)
+        # Neighborhood exists in the database, so return the rent information
+        return render(request, "rent_info.html", {'neighbourhood_info': neighbourhood_info})
+    except Neighbourhood_Info.DoesNotExist:
+        # Neighborhood doesn't exist in the database, so run the scraper function
+        scraped_data = get_avg_rent(city="Bangalore", bhk="2", locality=map_id)
+
+        if scraped_data is not None:
+            # Create a new NeighbourhoodInfo object and save it to the database
+            neighbourhood_info = Neighbourhood_Info.objects.create(neighbourhood=map_id, average_rent=scraped_data)
+            neighbourhood_info.save()
+            return render(request, "rent_info.html", {'neighbourhood_info': neighbourhood_info})
+        else:
+            # Handle the case where scraping failed
+            return HttpResponse("Scraping failed for this neighborhood")
+
+
